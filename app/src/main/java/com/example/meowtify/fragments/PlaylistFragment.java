e1029f71e99bbfd6b3634edab60f3b8fc9a0809b
Sergi <sergi.granell.7e3@itb.cat>
2021-03-31 23:33:39 +0200

    - completed xml and java of PlaylistFragment - fixed Utilitis on the navigation to reproductor. - adapt of adapters to use new navigation method of Utilitis - make GeneralItem serializable

--- app/src/main/java/com/example/meowtify/fragments/PlaylistFragment.java
+++ app/src/main/java/com/example/meowtify/fragments/PlaylistFragment.java
@@ -1,14 +1,27 @@
 package com.example.meowtify.fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.meowtify.R;
+import com.example.meowtify.adapters.AdapterSongsList;
+import com.example.meowtify.models.Followers;
+import com.example.meowtify.models.GeneralItem;
+import com.example.meowtify.models.Playlist;
+import com.example.meowtify.models.Type;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,6 +38,12 @@ public class PlaylistFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    ImageView imagePlaylist;
+    TextView namePlaylist, subtitelPlaylist;
+    Button buttonShuffel, buttonFolllow;
+    RecyclerView songs;
+    AdapterSongsList adapterSongs;
 
     public PlaylistFragment() {
         // Required empty public constructor
@@ -60,7 +79,51 @@ public class PlaylistFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_playlist, container, false);
+        View v = inflater.inflate(R.layout.fragment_playlist, container, false);
+
+        imagePlaylist = v.findViewById(R.id.image_playlist);
+        namePlaylist = v.findViewById(R.id.name_playlist);
+        subtitelPlaylist = v.findViewById(R.id.subname_playlist);
+        buttonShuffel = v.findViewById(R.id.shuffel_playlist);
+        buttonFolllow = v.findViewById(R.id.follow_playlist);
+        songs = v.findViewById(R.id.songs);
+        Playlist playlist = new Playlist(false, new Followers(), null, null, null, null, null, null, true, null, null, null);
+
+        Bundle b = getArguments();
+        if(b != null){
+            GeneralItem generalItem = (GeneralItem) b.getSerializable("generalItem");
+
+            //todo: get playlist from api with id.
+        }
+
+        namePlaylist.setText(playlist.getName());
+        String subtitel = "BY "+ playlist.getOwner() + " · " + playlist.getFollowers().getTotal() + " FOLLOWERS";
+        subtitelPlaylist.setText(subtitel);
+
+        buttonShuffel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //todo: do funcion navigation
+            }
+        });
+        buttonFolllow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //todo: add to the follow playlist
+            }
+        });
+
+        List<GeneralItem> songsList = new ArrayList<GeneralItem>(Arrays.asList(
+                new GeneralItem("7vlM4bn4gPubcmntK8UBp0", "Beliver", Type.track, "https://i.scdn.co/image/0f057142f11c251f81a22ca639b7261530b280b2", "artist11", null),
+                new GeneralItem("6Ynd3UhOWONEzAC2PtWGXw", "Beliver", Type.track, "https://i.scdn.co/image/0f057142f11c251f81a22ca639b7261530b280b2", "artist12", null),
+                new GeneralItem("0CeV1QZH5267PmzIpqRZmS", "Beliver", Type.track, "https://i.scdn.co/image/0f057142f11c251f81a22ca639b7261530b280b2", "artist13", null),
+                new GeneralItem("1wuW57ULEfM9pgCYIhROMs", "Beliver", Type.track, "https://i.scdn.co/image/0f057142f11c251f81a22ca639b7261530b280b2", "artist14", null)
+        ));
+
+        adapterSongs = new AdapterSongsList(songsList, getContext());
+        songs.setAdapter(adapterSongs);
+        songs.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        return v;
     }
 }
