b207d09fd3656af756abd04ff0a9fe4e1b12753b
Sergi <sergi.granell.7e3@itb.cat>
2021-03-31 15:06:56 +0200

    -search layout compleat - ic_baseline_close_24.xml imageBotton to eliminate a search of recentlySearch list - items layout of searchrRecycler and RecentlySearchRecycler done

--- app/src/main/java/com/example/meowtify/fragments/SearchFragment.java
+++ app/src/main/java/com/example/meowtify/fragments/SearchFragment.java
@@ -1,7 +1,9 @@
 package com.example.meowtify.fragments;
 
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.core.graphics.drawable.DrawableCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.meowtify.R;
+import com.google.android.material.textfield.TextInputLayout;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -60,7 +63,14 @@ public class SearchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View v = inflater.inflate(R.layout.fragment_search, container, false);
+
+        TextInputLayout t = v.findViewById(R.id.search_edit);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            DrawableCompat.setTint( t.getEndIconDrawable(), v.getContext().getColor(R.color.white));
+        }
+
+        return v;
     }
 }
