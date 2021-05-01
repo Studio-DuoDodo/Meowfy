df896568d1616ca0be217bfec2866123d26d5c48
Sergi <sergi.granell.7e3@itb.cat>
2021-05-01 23:47:38 +0200

    fixed isunes with notifications of the reproductor

--- app/src/main/java/com/example/meowtify/activities/MainActivity.java
+++ app/src/main/java/com/example/meowtify/activities/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.meowtify.activities;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.text.method.ScrollingMovementMethod;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,6 +20,7 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
@@ -41,6 +46,8 @@ import com.example.meowtify.services.ArtistService;
 import com.example.meowtify.services.MediaPlayerService;
 import com.example.meowtify.services.SongService;
 import com.example.meowtify.services.notifications.CreateNotification;
+import com.example.meowtify.services.notifications.OnClearFromRecentService;
+import com.example.meowtify.services.notifications.Playable;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.spotify.sdk.android.authentication.AuthenticationClient;
 import com.spotify.sdk.android.authentication.AuthenticationRequest;
@@ -50,9 +57,8 @@ import com.squareup.picasso.Picasso;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.function.Consumer;
 
-public class MainActivity extends AppCompatActivity implements OnFragmentChanged {
+public class MainActivity extends AppCompatActivity implements OnFragmentChanged, Playable {
     private static final String CLIENT_ID = "8175f0284ba94a128cca4b9d788449a6";
     private static final String REDIRECT_URI = "http://com.example.meowtify/callback";
     // private static final String REDIRECT_URI = " com.example.meowtify://callback";
@@ -61,6 +67,8 @@ public class MainActivity extends AppCompatActivity implements OnFragmentChanged
     public static boolean inReproductorForFirstTime = false;
     public static Fragment currentFragment;
     public static OnFragmentChanged onFragmentChanged;
+    public List<Song> songs = new ArrayList<>();
+    public int position;
     BottomNavigationView navigationMenu;
     ImageView bottomSheetImage;
     ReproductorFragment viewer = null;
@@ -80,11 +88,29 @@ public class MainActivity extends AppCompatActivity implements OnFragmentChanged
     private AlbumService albumService;
     private ArrayList<Song> recentlyPlayedTracks;
 
-
     //bottomSheetVariables
     private Type songType;
     private String idListSong;
-    private int posSong;
+    NotificationManager notificationManager;
+    public Intent mediaPlayerServiceIntent;
+    public boolean mBounded;
+    public MediaPlayerService mediaPlayerService;
+    public ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            Toast.makeText(getApplicationContext(), "Service is disconnected", Toast.LENGTH_LONG).show();
+            mBounded = false;
+            mediaPlayerService = null;
+        }
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            Toast.makeText(getApplicationContext(), "Service is connected", Toast.LENGTH_LONG).show();
+            mBounded = true;
+            MediaPlayerService.LocalBinder mLocalBinder = (MediaPlayerService.LocalBinder) service;
+            mediaPlayerService = mLocalBinder.getServerInstance();
+        }
+    };
 
     private View.OnClickListener addListener = v -> {
         songService.addSongToLibrary(this.song);
@@ -126,20 +152,24 @@ public class MainActivity extends AppCompatActivity implements OnFragmentChanged
     BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-
+            System.out.println("brodcast");
             String action = intent.getExtras().getString("actionname");
             switch (action) {
                 case CreateNotification.ACTION_PREVIUOS:
                     onTrackPrevious();
+                    System.out.println("previus");
                     break;
                 case CreateNotification.ACTION_PLAY:
-                    if (isPlaying) {
+                    if (MediaPlayerService.isPlaying()) {
+                        System.out.println("pause");
                         onTrackPause();
                     } else {
+                        System.out.println("play");
                         onTrackPlay();
                     }
                     break;
                 case CreateNotification.ACTION_NEXT:
+                    System.out.println("next");
                     onTrackNext();
                     break;
             }
@@ -151,7 +181,7 @@ public class MainActivity extends AppCompatActivity implements OnFragmentChanged
             NotificationChannel channel = new NotificationChannel(CreateNotification.CHANNEL_ID,
                     "Meowfy", NotificationManager.IMPORTANCE_LOW);
 
-            notificationManager = getActivity().getSystemService(NotificationManager.class);
+            notificationManager = getSystemService(NotificationManager.class);
             if (notificationManager != null) {
                 notificationManager.createNotificationChannel(channel);
             }
@@ -220,7 +250,7 @@ public class MainActivity extends AppCompatActivity implements OnFragmentChanged
                 GeneralItem gi = ReproductorFragment.songService.lastSearchedSong.toGeneralItem();
 
                 gi.setId(idListSong);
-                gi.setExtra1(String.valueOf(posSong));
+                gi.setExtra1(String.valueOf(position));
                 gi.setExtra2(songType.toString());
 
                 System.out.println(gi.toString());
@@ -232,12 +262,13 @@ public class MainActivity extends AppCompatActivity implements OnFragmentChanged
             @Override
             public void onClick(View v) {
                 if (MediaPlayerService.isPlaying()){
-                    MediaPlayerService.pause();
-                    playButton.setImageDrawable(getDrawable(android.R.drawable.ic_media_play));
-
+                    onTrackPause();
+                    /*MediaPlayerService.pause();
+                    playButton.setImageDrawable(getDrawable(android.R.drawable.ic_media_play));*/
                 }else {
-                MediaPlayerService.resume();
-                    playButton.setImageDrawable(getDrawable(android.R.drawable.ic_media_pause));
+                    onTrackPlay();
+                    /*MediaPlayerService.resume();
+                    playButton.setImageDrawable(getDrawable(android.R.drawable.ic_media_pause));*/
                 }}
         });
         /*songService = new SongService(getApplicationContext());
@@ -250,6 +281,12 @@ public class MainActivity extends AppCompatActivity implements OnFragmentChanged
         addBtn.setOnClickListener(addListener);*/
 
 
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createChannel();
+            registerReceiver(broadcastReceiver, new IntentFilter("TRACKS_TRACKS"));
+            getApplicationContext().startService(new Intent(getApplicationContext(), OnClearFromRecentService.class));
+        }
     }
 
 
@@ -324,8 +361,7 @@ public class MainActivity extends AppCompatActivity implements OnFragmentChanged
             if(ReproductorFragment.type == Type.track) songType = Type.album;
             else songType = ReproductorFragment.type;
             idListSong = ReproductorFragment.idList;
-            posSong = ReproductorFragment.position;
-            System.out.println("type lista "+songType+"\n id lista "+idListSong+"\nposicion lista "+ReproductorFragment.position);
+            System.out.println("type lista "+songType+"\n id lista "+idListSong+"\nposicion lista "+ position);
 
             Song s = ReproductorFragment.songService.lastSearchedSong;
             songTitle.setText(s.getName());
@@ -345,5 +381,85 @@ public class MainActivity extends AppCompatActivity implements OnFragmentChanged
             }
         }
 
+    }
+
+    @Override
+    public void onTrackPrevious() {
+        if (position - 1 > 0) {
+            position--;
+            ChangeSong(songs.get(position));
+            CreateNotification.createNotification(getApplicationContext(), songs.get(position), android.R.drawable.ic_media_pause, position, songs.size() - 1);
+        } else {
+            Toast.makeText(getApplicationContext(), "No more Songs", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onTrackPlay() {
+        if(inReproductorForFirstTime){
+            CreateNotification.createNotification(getApplicationContext(), songs.get(position),
+                    android.R.drawable.ic_media_pause, position, songs.size() - 1);
+            MediaPlayerService.resume();
+            playButton.setImageDrawable(getDrawable(android.R.drawable.ic_media_pause));
+        }
+    }
+
+    @Override
+    public void onTrackPause() {
+        if(inReproductorForFirstTime){
+            CreateNotification.createNotification(getApplicationContext(), songs.get(position),
+                    android.R.drawable.ic_media_play, position, songs.size() - 1);
+            MediaPlayerService.pause();
+            playButton.setImageDrawable(getDrawable(android.R.drawable.ic_media_play));
+        }
+    }
+
+    @Override
+    public void onTrackNext() {
+        if (position + 1 < songs.size()) {
+            position++;
+            CreateNotification.createNotification(getApplicationContext(), songs.get(position),
+                    android.R.drawable.ic_media_pause, position, songs.size() - 1);
+
+            ChangeSong(songs.get(position));
+
+        } else {
+            Toast.makeText(getApplicationContext(), "No more Songs", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onTrackEnd() {
+        onTrackNext();
+    }
+
+    private void updateSongByAPI() {
+        getApplicationContext().startService(mediaPlayerServiceIntent);
+        getApplicationContext().bindService(mediaPlayerServiceIntent, mConnection, BIND_AUTO_CREATE);
+
+        song = songs.get(position);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            getApplicationContext().startService(new Intent(getApplicationContext(), OnClearFromRecentService.class));
+        }
+
+        if (mBounded)
+            mediaPlayerService.changeSong(song);
+        onTrackPlay();
+
+        songTitle.setText(song.getName());
+        songTitle.setSelected(true);
+        subtitel.setText(song.getArtists().get(0).getName());
+        Picasso.with(getApplicationContext()).load(song.getAlbum().getImages().get(0).url).into(bottomSheetImage);
+    }
+
+    public void ChangeSong(Song s) {
+        if(!currentFragment.getTag().equals("Reproductor")){
+            songService.getASongByRef(this::updateSongByAPI, s.getId());
+        }else{
+            ReproductorFragment reproductor = ((ReproductorFragment) getSupportFragmentManager().findFragmentByTag("Reproductor"));
+
+            reproductor.ChangeSong(s);
+        }
     }
 }
